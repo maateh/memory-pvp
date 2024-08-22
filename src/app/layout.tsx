11d694686918ed1a5d7a35d75c56c32b18cf1b65
maateh <39100142+maateh@users.noreply.github.com>
2024-08-22 15:37:11 +0200

    update protected routes + auth options

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,10 @@ export const metadata: Metadata = {
 
 const BaseLayout = ({ children }: React.PropsWithChildren) => {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      signInFallbackRedirectUrl="/dashboard"
+      signUpFallbackRedirectUrl="/dashboard"
+    >
       <html lang="en" suppressHydrationWarning>
         <body className={cn("min-h-screen bg-background font-sans antialiased", font.variable)}>
           <ThemeProvider
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,10 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
 const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
-  '/sign-up(.*)'
+  '/sign-up(.*)',
+  '/',
+  '/toplist',
+  '/game(.*)'
 ])
 
 export default clerkMiddleware((auth, request) => {
