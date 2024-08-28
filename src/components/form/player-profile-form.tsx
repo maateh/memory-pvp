2a2e0ed48f152a5f8e7de5731a49c8338427a1f8
maateh <39100142+maateh@users.noreply.github.com>
2024-08-28 19:19:49 +0200

    feat: create player profile form

---
+++ src/components/form/player-profile-form.tsx
@@ -0,0 +1,83 @@
+import { z } from "zod"
+
+// icons
+import { Check, Square } from "lucide-react"
+
+// shadcn
+import { Button } from "@/components/ui/button"
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+// components
+import { Form } from "@/components/form"
+
+type PlayerProfileFormValues = z.infer<typeof playerProfileFormSchema>
+
+const playerProfileFormSchema = z.object({
+  playerName: z.string(),
+  color: z.string()
+})
+
+const PlayerProfileForm = () => {
+  const onSubmit = async (values: PlayerProfileFormValues) => {
+    // TODO: create player profile
+    console.log({ values })
+  }
+
+  return (
+    <Form<PlayerProfileFormValues>
+      className="flex items-center gap-x-4"
+      schema={playerProfileFormSchema}
+      defaultValues={{
+        playerName: '',
+        color: ''
+      }}
+      onSubmit={onSubmit}
+    >
+      {({ control }) => (
+        <>
+          <FormField
+            control={control}
+            name="playerName"
+            render={({ field }) => (
+              <FormItem className="flex-1">
+                <FormLabel>
+                  Player Name
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={control}
+            name="color"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  {/* TODO: color select */}
+                  <Square className="size-9" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button className="ml-3 p-1.5 hover:bg-transparent/5 dark:hover:bg-transparent/5"
+            variant="ghost"
+            size="icon"
+          >
+            <Check className="size-5 text-accent"
+              strokeWidth={4}
+            />
+          </Button>
+        </>
+      )}
+    </Form>
+  )
+}
+
+export default PlayerProfileForm
--- src/components/widget/modals/player-profiles-widget-modal.tsx
+++ src/components/widget/modals/player-profiles-widget-modal.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 // components
+import { PlayerProfileForm } from "@/components/form"
+import { Separator } from "@/components/ui/separator"
 import { WidgetModal } from "@/components/widget"
 
 // hooks
@@ -12,7 +14,21 @@ const PlayerProfilesWidgetModal = () => {
 
   return (
     <WidgetModal isOpen={isModalOpen} {...data}>
-      PlayerProfiles
+      <div className="space-y-8">
+        <h4 className="text-lg font-heading font-bold small-caps">
+          Create a new player profile
+        </h4>
+
+        <PlayerProfileForm />
+      </div>
+
+      <Separator className="w-11/12 my-6 mx-auto bg-border/60" />
+
+      <h4 className="text-lg font-heading font-bold small-caps">
+        Manage your current profiles
+      </h4>
+      
+      TODO: profile list
     </WidgetModal>
   )
 }
