6b17157254dcc097215124410737a3002e24427f
maateh <39100142+maateh@users.noreply.github.com>
2024-08-31 22:04:06 +0200

    feat(ui): show user info on manage account widget

--- src/components/widgets/manage-account/card.tsx
+++ src/components/widgets/manage-account/card.tsx
@@ -1,13 +1,19 @@
 "use client"
 
+import Image from "next/image"
+
 // clerk
 import { useClerk } from "@clerk/nextjs"
 
 // prisma
 import { User } from "@prisma/client"
 
+// icons
+import { Mail } from "lucide-react"
+
 // components
 import { WidgetCard, type WidgetInfo } from "@/components/widgets"
+import UserInfoItem from "./user-info-item"
 
 type ManageAccountWidgetCardProps = {
   user: User
@@ -21,8 +27,30 @@ const ManageAccountWidgetCard = ({ user, ...props }: ManageAccountWidgetCardProp
       widgetAction={openUserProfile}
       {...props}
     >
-      <div>
-        {user?.username}
+      <h4 className="mt-2 mb-5 w-fit border-t border-t-accent text-lg font-heading font-semibold small-caps">
+        Account details
+      </h4>
+
+      <div className="space-y-6">
+        <UserInfoItem
+          label="Username"
+          info={`@${user.username}`}
+          icon={(
+            <div className="size-8 rounded-2xl img-wrapper">
+              <Image
+                src={user.imageUrl} // FIXME: add fallback src
+                alt="user profile"
+                fill
+              />
+            </div>
+          )}
+        />
+
+        <UserInfoItem
+          label="Email"
+          info={user.email}
+          icon={<Mail className="size-7" />}
+        />
       </div>
     </WidgetCard>
   )
