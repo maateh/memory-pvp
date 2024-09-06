13b1f83c40149503d34757ba00af12cfa7c78ecf
maateh <39100142+maateh@users.noreply.github.com>
2024-09-06 16:57:50 +0200

    style: modify game layouts & setup page

--- src/app/game/[sessionId]/layout.tsx
+++ src/app/game/[sessionId]/layout.tsx
@@ -3,7 +3,7 @@ import { GameActionBar, GameSessionFooter } from "@/components/game"
 
 const GameSessionLayout = ({ children }: React.PropsWithChildren) => {
   return (
-    <div className="h-screen w-full mx-auto flex flex-col items-center gap-y-4 bg-foreground/5 md:max-w-screen-lg lg:max-w-screen-xl">
+    <div className="w-full mx-auto flex flex-col items-center gap-y-4 bg-foreground/10 rounded-xl">
       <GameActionBar />
 
       <div className="flex-1">
--- src/app/game/setup/page.tsx
+++ src/app/game/setup/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link"
+
+// prisma
+import { GameMode } from "@prisma/client"
+
+// constants
+import { gamemodes, tableSizes } from "@/constants/navigation"
+
+// utils
+import { cn } from "@/lib/utils"
+
+// icons
+import { LayoutDashboard } from "lucide-react"
+
+// shadcn
+import { Button, buttonVariants } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+
 type GameSetupPageProps = {
   searchParams: {
     mode: 'single' | 'pvp' | 'coop'
@@ -6,7 +24,72 @@ type GameSetupPageProps = {
 
 const GameSetupPage = ({ searchParams }: GameSetupPageProps) => {
   return (
-    <div>GameSetupPage</div>
+    <div className="flex-1 pt-16 pb-8 px-4 flex flex-col gap-y-3">
+      <div className="w-fit mx-auto text-center">
+        <h1 className="mx-auto text-3xl font-heading font-semibold heading-decorator sm:text-4xl">
+          Let&apos;s memorize!
+        </h1>
+
+        <p className="text-muted-foreground text-xl font-heading sm:text-2xl">
+          ...but first, <span className="text-accent">configure</span> your game session.
+        </p>
+      </div>
+
+      <Separator className="w-3/5 mx-auto my-6 sm:w-2/5 lg:w-1/5" />
+
+      <div className="space-y-2 text-center">
+        <h2 className="text-xl font-heading font-semibold small-caps">
+          Select Gamemode
+        </h2>
+
+        <div className="max-w-sm mx-auto grid gap-2 grid-cols-1 sm:grid-cols-2">
+          {gamemodes.map(({ key, label, Icon }) => (
+            <Button className="p-2.5 flex flex-wrap items-center justify-center gap-x-2 gap-y-1.5 border-2 border-background/85 bg-background/25 rounded-2xl sm:last-of-type:col-span-2"
+              variant="default"
+              size="icon"
+              disabled={key !== GameMode.SINGLE}
+              key={key}
+            >
+              <Icon className="size-5 shrink-0" />
+              <p className="text-lg small-caps">
+                {label}
+              </p>
+            </Button>
+          ))}
+        </div>
+      </div>
+
+      <div className="mt-12 space-y-2 text-center sm:mt-8">
+        <h2 className="text-xl font-heading font-semibold small-caps">
+          Table Size
+        </h2>
+
+        <div className="max-w-sm mx-auto grid gap-2 grid-cols-1 sm:grid-cols-2">
+          {tableSizes.map(({ key, label, Icon }) => (
+            <Button className="p-2.5 flex flex-wrap items-center justify-center gap-x-2 gap-y-1.5 border-2 border-background/85 bg-background/15 rounded-2xl sm:last-of-type:col-span-2"
+              variant="default"
+              size="icon"
+              key={key}
+            >
+              <Icon className="size-5 shrink-0" />
+              <p className="text-lg small-caps">
+                {label}
+              </p>
+            </Button>
+          ))}
+        </div>
+      </div>
+
+      <Link className={cn(buttonVariants({
+        className: "py-7 px-6 mx-auto mt-auto gap-x-2 text-base",
+        variant: "destructive"
+      }))}
+        href="/"
+      >
+        <LayoutDashboard />
+        Back to the homepage
+      </Link>
+    </div>
   )
 }
 
