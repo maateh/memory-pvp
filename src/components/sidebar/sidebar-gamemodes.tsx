fa92cdd00142f17d1ab2cb6bd4e8f76d252d3ba4
maateh <39100142+maateh@users.noreply.github.com>
2024-09-06 16:07:06 +0200

    feat: update constants

--- src/components/sidebar/sidebar-gamemodes.tsx
+++ src/components/sidebar/sidebar-gamemodes.tsx
@@ -14,14 +14,14 @@ const SidebarGamemodes = () => {
       </h2>
 
       <div className="max-w-xl mx-auto grid grid-cols-2 gap-2">
-        {gamemodes.map(({ label, href, Icon }) => (
+        {gamemodes.map(({ key, label, query, Icon }) => (
           <Link className={buttonVariants({
             className: "px-2.5 py-1.5 flex flex-wrap items-center justify-center gap-x-2 gap-y-1.5 border-2 rounded-xl last-of-type:col-span-2",
             variant: "outline",
             size: "icon"
           })}
-            href={href}
-            key={href}
+            href={`/game/setup?${query}`}
+            key={key}
           >
             <Icon className="size-5 shrink-0" />
             <p className="text-lg small-caps">
--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,21 +1,48 @@
+// prisma
+import { GameMode, TableSize } from "@prisma/client"
+
 // icons
-import { ChartColumn, Gamepad2, LayoutDashboard, Swords, UserCircle, UsersRound } from "lucide-react"
+import { ChartColumn, Dice4, Dice5, Dice6, Gamepad2, LayoutDashboard, Swords, UserCircle, UsersRound } from "lucide-react"
 
 export const gamemodes = [
   {
+    key: GameMode.SINGLE,
     label: 'Single',
-    href: '/game/setup?mode=single',
+    query: 'mode=single',
     Icon: Gamepad2
   },
   {
+    key: GameMode.PVP,
     label: 'PvP',
-    href: '/game/setup?mode=pvp',
+    query: 'mode=pvp',
     Icon: Swords
   },
   {
+    key: GameMode.COOP,
     label: 'Co-Op',
-    href: '/game/setup?mode=coop',
+    query: 'mode=coop',
     Icon: UsersRound
+  }
+]
+
+export const tableSizes = [
+  {
+    key: TableSize.SMALL,
+    label: 'Small (4x4)',
+    query: 'size=small',
+    Icon: Dice4
+  },
+  {
+    key: TableSize.MEDIUM,
+    label: 'Small (5x5)',
+    query: 'size=medium',
+    Icon: Dice5
+  },
+  {
+    key: TableSize.LARGE,
+    label: 'Small (6x6)',
+    query: 'size=large',
+    Icon: Dice6
   }
 ]
 
