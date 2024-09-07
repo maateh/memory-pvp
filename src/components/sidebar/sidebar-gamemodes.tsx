eea9812a6ed7a9110d0255ced8238c72a7faf136
maateh <39100142+maateh@users.noreply.github.com>
2024-09-07 15:12:21 +0200

    refactor: replace game modes to types on sidebar

--- src/components/sidebar/sidebar-gamemodes.tsx
+++ src/components/sidebar/sidebar-gamemodes.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 
 // constants
-import { gameModes } from "@/constants/game"
+import { gameTypes } from "@/constants/game"
 
 // shadcn
 import { buttonVariants } from "@/components/ui/button"
@@ -9,22 +9,22 @@ import { buttonVariants } from "@/components/ui/button"
 const SidebarGamemodes = () => {
   return (
     <div className="space-y-1.5">
-      <h2 className="text-xl font-bold small-caps">
-        Select a gamemode
+      <h2 className="text-xl font-heading font-bold small-caps">
+        Start New Game
       </h2>
 
-      <div className="max-w-xl mx-auto grid grid-cols-2 gap-2">
-        {gameModes.map(({ key, label, query, Icon }) => (
+      <div className="flex flex-wrap gap-2">
+        {gameTypes.map(({ key, label, Icon }) => (
           <Link className={buttonVariants({
-            className: "px-2.5 py-1.5 flex flex-wrap items-center justify-center gap-x-2 gap-y-1.5 border-2 rounded-xl last-of-type:col-span-2",
-            variant: "outline",
+            className: "w-full flex-1 px-2.5 py-2 flex items-center justify-center gap-x-2 border-2 border-background/85 last-of-type:odd:col-span-2",
+            variant: "ghost",
             size: "icon"
           })}
-            href={`/game/setup?${query}`}
+            href={`/game/setup?type=${key}`}
             key={key}
           >
-            <Icon className="size-5 shrink-0" />
-            <p className="text-lg small-caps">
+            <Icon className="size-[1.125rem] shrink-0" />
+            <p className="text-base small-caps">
               {label}
             </p>
           </Link>
--- src/constants/game.ts
+++ src/constants/game.ts
@@ -8,13 +8,11 @@ export const gameTypes = [
   {
     key: GameType.CASUAL,
     label: 'Casual',
-    query: 'mode=casual',
     Icon: Gamepad2
   },
   {
     key: GameType.COMPETITIVE,
     label: 'Competitive',
-    query: 'mode=competitive',
     Icon: Swords
   }
 ]
@@ -23,19 +21,16 @@ export const gameModes = [
   {
     key: GameMode.SINGLE,
     label: 'Single',
-    query: 'mode=single',
     Icon: Gamepad2
   },
   {
     key: GameMode.PVP,
     label: 'PvP',
-    query: 'mode=pvp',
     Icon: Swords
   },
   {
     key: GameMode.COOP,
     label: 'Co-Op',
-    query: 'mode=coop',
     Icon: UsersRound
   }
 ]
@@ -44,19 +39,16 @@ export const tableSizes = [
   {
     key: TableSize.SMALL,
     label: 'Small (4x4)',
-    query: 'size=small',
     Icon: Dice4
   },
   {
     key: TableSize.MEDIUM,
     label: 'Small (5x5)',
-    query: 'size=medium',
     Icon: Dice5
   },
   {
     key: TableSize.LARGE,
     label: 'Small (6x6)',
-    query: 'size=large',
     Icon: Dice6
   }
 ]
