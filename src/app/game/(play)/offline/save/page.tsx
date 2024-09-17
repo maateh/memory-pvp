c3963173178dd666a3c532e690f0ecae1dc97a67
maateh <39100142+maateh@users.noreply.github.com>
2024-09-17 19:55:55 +0200

    style: modify save layout margins & paddings

--- src/app/game/(play)/offline/save/page.tsx
+++ src/app/game/(play)/offline/save/page.tsx
@@ -28,13 +28,13 @@ const GameOfflineSavePage = async () => {
   }
 
   return (
-    <main className="flex-1 px-2.5 space-y-16 sm:px-4">
-      <header className="mt-24 space-y-3">
-        <h1 className="w-fit pt-2 mx-auto text-3xl font-heading font-bold small-caps heading-decorator sm:text-5xl">
+    <main className="flex-1 px-2.5 sm:px-4">
+      <header className="mt-24 mb-12">
+        <h1 className="w-fit pt-1.5 mx-auto text-3xl font-heading font-bold small-caps heading-decorator sm:text-5xl">
           Save Offline Session
         </h1>
 
-        <Separator className="w-5/6 mx-auto border-foreground/50" />
+        <Separator className="w-5/6 mx-auto my-3 border-foreground/50" />
 
         <p className="max-w-lg mx-auto text-center font-heading text-base sm:text-lg">
           You&apos;re game is over, thanks for playing!
