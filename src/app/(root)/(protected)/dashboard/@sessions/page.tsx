bee89ae9f64674b5a2713f9e17fce30d4c44e98f
maateh <39100142+maateh@users.noreply.github.com>
2024-09-18 19:31:33 +0200

    style: create basic layouts for session summary

--- src/app/(root)/(protected)/dashboard/@sessions/page.tsx
+++ src/app/(root)/(protected)/dashboard/@sessions/page.tsx
@@ -1,16 +1,60 @@
 // constants
-import { gameSessionsWidgetInfo } from '@/components/widgets/constants'
+import { gameSessionsWidgetInfo } from "@/components/widgets/constants"
+
+// icons
+import { ChevronRightCircle, SquareSigma } from "lucide-react"
+
+// shadcn
+import { Badge } from "@/components/ui/badge"
 
 // components
-import { WidgetCard } from '@/components/widgets'
+import { WidgetCard } from "@/components/widgets"
 
 const SessionsWidgetCard = () => {
+  // TODO: show played sessions per categories
+  // A "breadcrumb"-like filter mechanism
+
+  // CASUAL sessions (+ offline played)
+  // - [SINGLE] sessions (+ offline played)
+  //    - [...TABLE_SIZES] sessions (+ offline played)
+  // - [COOP] sessions
+  //    - [...TABLE_SIZES] sessions (+ offline played)
+
+  // COMPETITIVE sessions
+  // - [SINGLE] sessions
+  //    - [...TABLE_SIZES] sessions
+  // - [COOP] sessions
+  //    - [...TABLE_SIZES] sessions
+  // - [PVP] sessions
+  //    - [...TABLE_SIZES] sessions
+
   return (
     <WidgetCard
       widgetLink="/dashboard/sessions"
       {...gameSessionsWidgetInfo}
     >
-      
+      <h4 className="mt-2 mb-3 w-fit border-t border-t-accent text-lg font-heading font-semibold small-caps">
+        Summary of your sessions
+      </h4>
+
+      <div className="flex items-center gap-x-1.5">
+        <Badge>
+          Casual
+        </Badge>
+
+        <Badge>
+          Competitive
+        </Badge>
+
+        <ChevronRightCircle className="size-4 text-foreground/65" />
+      </div>
+
+      <div className="mt-5 pt-2.5 flex gap-x-2 border-t border-border/70">
+        <SquareSigma className="size-4 sm:size-5" />
+        <p className="font-heading small-caps">
+          Total sessions: <span className="font-medium">420</span>
+        </p>
+      </div>
     </WidgetCard>
   )
 }
---
+++ src/app/game/summary/[sessionId]/page.tsx
@@ -0,0 +1,35 @@
+// server
+import { db } from "@/server/db"
+import { signedIn } from "@/server/actions/signed-in"
+
+// shadcn
+import { Separator } from "@/components/ui/separator"
+import { SessionStats } from "@/components/session"
+
+type GameSessionSummaryProps = {
+  params: {
+    sessionId: string
+  }
+}
+
+const GameSessionSummary = async ({ params }: GameSessionSummaryProps) => {
+  // TODO: fetch session
+  const session = null
+  if (!session) return
+
+  return (
+    <main className="flex-1 px-2.5 sm:px-4">
+      <header className="mt-24 mb-12">
+        <h1 className="mt-24 pt-1.5 w-fit mx-auto text-3xl font-heading font-bold small-caps heading-decorator sm:text-5xl">
+          Session Results
+        </h1>
+
+        <Separator className="w-5/6 mx-auto my-3 border-foreground/50" />
+      </header>
+
+      <SessionStats session={session} />
+    </main>
+  )
+}
+
+export default GameSessionSummary
