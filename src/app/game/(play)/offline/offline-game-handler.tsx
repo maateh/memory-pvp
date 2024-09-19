ded96164ade5723addc1e72ad9c7fd2754ba8791
maateh <39100142+maateh@users.noreply.github.com>
2024-09-19 13:47:40 +0200

    refactor: move offline game handler into a custom hook

--- src/app/game/(play)/offline/offline-game-handler.tsx
+++ src/app/game/(play)/offline/offline-game-handler.tsx
@@ -1,8 +1,5 @@
 "use client"
 
-import { useEffect, useState } from "react"
-import { redirect } from "next/navigation"
-
 // constants
 import { offlineSessionMetadata } from "@/constants/game"
 
@@ -11,70 +8,10 @@ import { SessionHeader } from "@/components/session"
 import { MemoryTable } from "@/components/session/game"
 
 // hooks
-import { type MemoryCard, useSessionStore } from "@/hooks/store/use-session-store"
-import { useOfflineSessionHandler } from "@/hooks/handler/session/use-offline-session-handler"
+import { useOfflineGameHandler } from "@/hooks/handler/game/use-offline-game-handler"
 
 const OfflineGameHandler = () => {
-  /** Check if there is any client session. */
-  const clientSession = useSessionStore((state) => state.session)
-  if (!clientSession) redirect('/game/setup')
-
-  /** Get custom handler to finish the game. */
-  const { finishOfflineSession } = useOfflineSessionHandler()
-
-  /** Initialize required states and handlers for the game. */
-  const [cards, setCards] = useState<MemoryCard[]>(clientSession.cards)
-  const [flippedCards, setFlippedCards] = useState<MemoryCard[]>([])
-  
-  const updateSessionCards = useSessionStore((state) => state.updateCards)
-  const increaseSessionFlips = useSessionStore((state) => state.increaseFlips)
-
-  const handleCardFlip = (clickedCard: MemoryCard) => {
-    if (flippedCards.length === 2 || clickedCard.isMatched) return
-    increaseSessionFlips()
-    
-    const updatedCards = cards.map((card) =>
-      card.id === clickedCard.id ? { ...card, isFlipped: true } : card
-    )
-    setCards(updatedCards)
-
-    const flipped = [...flippedCards, clickedCard]
-    setFlippedCards(flipped)
-
-    if (flipped.length < 2) return
-
-    if (flipped[0].key === flipped[1].key) {
-      setTimeout(() => {
-        setCards((cards) => {
-          const updatedCards = cards.map(
-            (card) => card.key === flipped[0].key ? { ...card, isMatched: true } : card
-          )
-          updateSessionCards(updatedCards)
-          return updatedCards
-        })
-
-        setFlippedCards([])
-      }, 1000)
-    } else {
-      setTimeout(() => {
-        setCards((cards) => (
-          cards.map((card) =>
-            flipped.some(fc => fc.id === card.id)
-              ? { ...card, isFlipped: false }
-              : card
-          )
-        ))
-
-        setFlippedCards([])
-      }, 1000)
-    }
-  }
-
-  useEffect(() => {
-    if (cards.every(card => card.isMatched)) {
-      finishOfflineSession('FINISHED')
-    }
-  }, [cards, finishOfflineSession])
+  const { clientSession, cards, handleCardFlip } = useOfflineGameHandler()
 
   return (
     <>
