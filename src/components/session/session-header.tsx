f58f8d773872d372bfc55643401e655cd1d6f7c6
maateh <39100142+maateh@users.noreply.github.com>
2024-09-23 12:19:19 +0200

    feat: display timer in SessionHeader

--- src/components/session/session-header.tsx
+++ src/components/session/session-header.tsx
@@ -1,17 +1,25 @@
+// utils
+import { formatTimer } from "@/lib/utils/game"
+
 // components
 import { ThemeToggle } from "@/components/shared"
 import { SessionActionsDropdown } from "@/components/session"
+
+// hooks
+import { useTimer } from "@/hooks/use-timer"
 
 type SessionHeaderProps = {
   session: ClientGameSession
 }
 
 const SessionHeader = ({ session }: SessionHeaderProps) => {
+  const { timer } = useTimer({ initialInMs: session.timer * 1000 })
+
   return (
     <div className="w-full min-h-14 mx-auto py-3 px-1.5 flex items-center justify-between bg-primary sm:px-5 sm:rounded-b-2xl sm:max-w-lg md:max-w-xl">
       <SessionActionsDropdown session={session} />
 
-      <p className="text-lg">Timer</p>
+      <p className="text-lg">{formatTimer(timer)}</p>
 
       <ThemeToggle className="hover:bg-primary-foreground/5 hover:text-primary-foreground"
         variant="ghost"
--- src/lib/utils/game.ts
+++ src/lib/utils/game.ts
@@ -1,4 +1,5 @@
 import { v4 as uuidv4 } from "uuid"
+import { intervalToDuration } from "date-fns"
 
 // types
 import type { TableSize } from "@prisma/client"
@@ -6,6 +7,22 @@ import type { MemoryCard } from "@/hooks/store/use-session-store"
 
 // constants
 import { tableSizeMap } from "@/constants/game"
+
+export function formatTimer(timerInMs: number) {
+  const duration = intervalToDuration({
+    start: 0,
+    end: timerInMs
+  })
+
+  const zeroPad = (num: number | undefined) => String(num).padStart(2, '0')
+
+  const hours = duration.hours ? zeroPad(duration.hours) : '00'
+  const minutes = duration.minutes ? zeroPad(duration.minutes) : '00'
+  const seconds = duration.seconds ? zeroPad(duration.seconds) : '00'
+
+  if (duration.hours) return `${hours}:${minutes}:${seconds}`
+  return `${minutes}:${seconds}`
+}
 
 export const getMockCards = (tableSize: TableSize): MemoryCard[] => {
   const cards: MemoryCard[] = []
