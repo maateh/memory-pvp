6e94f5976f6ce4d688e27753c150f4353239ab9a
maateh <39100142+maateh@users.noreply.github.com>
2024-09-28 11:11:19 +0200

    feat: add useAbandonSessionMutation
    This hook uses the 'session.save' API endpoint because it'll be used from the game navbar where we want to abandon, but also save the session.

--- src/lib/react-query/mutations/game/index.ts
+++ src/lib/react-query/mutations/game/index.ts
@@ -1,3 +1,4 @@
 export { useStartSessionMutation } from "./use-start-session"
 export { useFinishSessionMutation } from "./use-finish-session"
+export { useAbandonSessionMutation } from "./use-abandon-session"
 export { useSaveOfflineSessionMutation } from "./use-save-offline-session"
