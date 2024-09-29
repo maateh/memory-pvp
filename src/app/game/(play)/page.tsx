53711878d8533b0a67f72176da9b2b415417e366
maateh <39100142+maateh@users.noreply.github.com>
2024-09-29 15:27:59 +0200

    feat: implement page for single online games

--- src/app/game/(play)/page.tsx
+++ src/app/game/(play)/page.tsx
@@ -1,16 +1,79 @@
 "use client"
 
+import { useRouter } from "next/navigation"
+
+import { toast } from "sonner"
+
+// trpc
+import { api } from "@/trpc/client"
+
+// utils
+import { handleApiError, logError } from "@/lib/utils"
+
 // components
 import { SessionFooter, SessionHeader } from "@/components/session"
 import { MemoryTable } from "@/components/session/game"
 
 // hooks
 import { useGameHandler } from "@/hooks/handler/game/use-game-handler"
+import { useSessionStore } from "@/hooks/store/use-session-store"
 
 const GamePlayPage = () => {
+  const router = useRouter()
+
+  const storeSession = api.session.store.useMutation({
+    onSuccess: () => {
+      console.log('Success! (session stored)')
+
+      // TODO: A marker inside the SessionHeader would be useful
+      // to indicate the session is synchronized or not.
+    },
+    onError: (err) => {
+      handleApiError(err.shape?.cause, 'Failed to store game session.')
+    }
+  })
+  
+  const saveSession = api.session.save.useMutation({
+    onSuccess: ({ sessionId }) => {
+      router.replace(`/game/summary/${sessionId}`)
+
+      toast.success('You finished your game session!', {
+        description: 'Session data has been successfully saved.'
+      })
+    },
+    onError: (err) => {
+      handleApiError(err.shape?.cause, 'Failed to save game session.')
+    }
+  })
+
   const { clientSession, handleCardFlip } = useGameHandler({
-    finishSession: () => {
-      // TODO: implement
+    onHeartbeat: async () => {
+      try {
+        // TODO: another API endpoint is required which can be
+        // triggered by cron jobs to save stored sessions from
+        // redis to the mongodb database.
+
+        await storeSession.mutateAsync(clientSession)
+        useSessionStore.setState({ shouldStore: false })
+      } catch (err) {
+        logError(err)
+      }
+    },
+
+    onBeforeUnload: async () => {
+      const payload = JSON.stringify(clientSession)
+      navigator.sendBeacon('/api/session/closed', payload)
+    },
+    
+    onFinish: async () => {
+      try {
+        await saveSession.mutateAsync({
+          ...clientSession,
+          status: 'FINISHED'
+        })
+      } catch (err) {
+        logError(err)
+      }
     }
   })
 
@@ -20,7 +83,7 @@ const GamePlayPage = () => {
 
       <MemoryTable
         session={clientSession}
-        handleCardFlip={handleCardFlip} // TODO: implement
+        handleCardFlip={handleCardFlip}
       />
 
       <SessionFooter session={clientSession} />
