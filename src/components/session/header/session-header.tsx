f27bf8cf7391fe959cf84ae26a7c7db08e34e07e
maateh <39100142+maateh@users.noreply.github.com>
2024-10-04 07:56:34 +0200

    style: modify positioning in session header

--- src/components/session/header/session-header.tsx
+++ src/components/session/header/session-header.tsx
@@ -15,8 +15,8 @@ type SessionHeaderProps = {
 
 const SessionHeader = ({ session }: SessionHeaderProps) => {
   return (
-    <div className="relative w-full min-h-14 mx-auto py-3 px-1.5 flex items-center justify-between gap-x-5 bg-primary md:px-5 md:rounded-b-2xl md:max-w-2xl">
-      <div className="flex items-center gap-x-1.5 sm:gap-x-2 absolute sm:static">
+    <div className="relative w-full min-h-14 mx-auto py-3 px-2.5 flex items-center justify-between gap-x-5 bg-primary md:px-5 md:rounded-b-2xl md:max-w-2xl">
+      <div className="flex items-center gap-x-1.5 absolute sm:static">
         <SessionActionsDropdown session={session} />
 
         <Separator className="h-4 sm:h-5 w-1 bg-border/50 rounded-full"
@@ -26,16 +26,14 @@ const SessionHeader = ({ session }: SessionHeaderProps) => {
         <SessionBasics {...session} />
       </div>
 
-      <div className="mx-auto sm:mr-0 flex items-center justify-center gap-x-2 relative">
-        <div className="flex flex-row-reverse sm:flex-row items-center gap-x-2 absolute sm:static inset-y-1/2 -right-8">
-          <SessionSyncMarker />
-
-          <Separator className="h-4 sm:h-[1.125rem] w-0.5 rounded-full"
-            orientation="vertical"
-          />
-        </div>
-
+      <div className="flex items-center justify-center gap-x-2 ml-auto sm:flex-row-reverse">
         <SessionTimer initialInMs={session.timer * 1000} />
+
+        <Separator className="h-4 sm:h-[1.125rem] w-1 bg-border/50 rounded-full"
+          orientation="vertical"
+        />
+
+        <SessionSyncMarker />
       </div>
     </div>
   )
--- src/components/session/header/session-timer.tsx
+++ src/components/session/header/session-timer.tsx
@@ -8,10 +8,12 @@ const SessionTimer = ({ initialInMs }: { initialInMs: number }) => {
   const { timer } = useTimer({ initialInMs })
 
   return (
-    <div className="pt-1.5 pb-0.5 px-4 bg-accent/25 dark:bg-accent/15 rounded-2xl">
-      <p className="text-xl sm:text-2xl font-heading font-bold tracking-wider">
-        {formatTimer(timer)}
-      </p>
+    <div className="absolute top-1/2 left-1/2 sm:static max-sm:transform max-sm:-translate-x-1/2 max-sm:-translate-y-1/2">
+      <div className="pt-1.5 pb-0.5 px-4 bg-accent/25 dark:bg-accent/15 rounded-2xl">
+        <p className="text-xl sm:text-2xl font-heading font-bold tracking-wider">
+          {formatTimer(timer)}
+        </p>
+      </div>
     </div>
   )
 }
