3c14b20eee3937bd356ed3d31c0da9fe34e3f07e
maateh <39100142+maateh@users.noreply.github.com>
2024-10-05 18:55:12 +0200

    refactor: replace getPlayers query

--- src/components/session/footer/session-footer.tsx
+++ src/components/session/footer/session-footer.tsx
@@ -1,11 +1,5 @@
 "use client"
 
-// trpc
-import { api } from "@/trpc/client"
-
-// utils
-import { cn } from "@/lib/utils"
-
 // shadcn
 import { Separator } from "@/components/ui/separator"
 
@@ -17,22 +11,14 @@ type SessionFooterProps = {
 }
 
 const SessionFooter = ({ session }: SessionFooterProps) => {
-  const { data: players, isLoading } = api.session.getPlayers.useQuery()
-
   return (
-    <div className={cn("w-full min-h-16 mx-auto py-3 px-3 flex flex-col items-center justify-center gap-x-6 bg-primary md:px-6 md:flex-row md:rounded-t-3xl md:max-w-screen-md lg:max-w-[896px]", {
-      "sm:px-6 md:px-10": session.mode === 'SINGLE'
-    })}>
-      {players && !isLoading ? (
-        <SessionPlayer
-          player={players?.owner}
-          session={session}
-        />
-      ) : (
-        <SessionPlayerSkeleton />
-      )}
+    <div className="w-full min-h-16 mx-auto py-3 px-3 flex flex-col items-center justify-center gap-x-4 bg-primary md:px-6 md:flex-row md:rounded-t-3xl md:max-w-screen-md lg:max-w-[896px]">
+      <SessionPlayer
+        player={session.players.current}
+        session={session}
+      />
 
-      {session.mode !== 'SINGLE' && players?.guest && (
+      {session.mode !== 'SINGLE' && session.players.other && (
         <>
           <Separator className="flex w-4/5 h-1 mx-auto my-4 bg-secondary-foreground/80 rounded-full md:hidden"
             orientation="vertical"
@@ -43,7 +29,7 @@ const SessionFooter = ({ session }: SessionFooterProps) => {
           />
 
           <SessionPlayer
-            player={players.guest}
+            player={session.players.other}
             session={session}
             flipOrder
           />
