5e10dd93f273faa6b137b62e555bfe602364bcf0
maateh <39100142+maateh@users.noreply.github.com>
2024-10-05 18:59:17 +0200

    feat!: remove use-abandon-session mutation hook

--- src/lib/react-query/mutations/game/index.ts
+++ src/lib/react-query/mutations/game/index.ts
@@ -1,3 +1,2 @@
 export { useStartSessionMutation } from "./use-start-session"
-export { useAbandonSessionMutation } from "./use-abandon-session"
 export { useSaveOfflineSessionMutation } from "./use-save-offline-session"
