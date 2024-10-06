10ebf61387009287ecb0b9b5dc1194f641e754b3
maateh <39100142+maateh@users.noreply.github.com>
2024-10-06 08:13:13 +0200

    feat: add 'pickFields' util

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -9,6 +9,27 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 /**
+ * Selectively picks specified fields from an object.
+ * 
+ * - Takes an object and an array of keys, returning a new object with only the specified fields.
+ * - If a key doesn't exist in the object, it is ignored.
+ * 
+ * @param {T} obj - The object to pick fields from.
+ * @param {U[]} keys - An array of keys to extract from the object.
+ * 
+ * @returns {Pick<T, U>} - A new object containing only the specified fields.
+ */
+export function pickFields<T extends object, U extends keyof T>(obj: T, keys: U[]): Pick<T, U> {
+  const result = {} as Pick<T, U>
+  keys.forEach(key => {
+    if (key in obj) {
+      result[key] = obj[key]
+    }
+  })
+  return result
+}
+
+/**
  * Handles API errors by displaying an appropriate toast notification.
  * 
  * @param {TRPCApiError | undefined} err - The error object from the API, if available.
