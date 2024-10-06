2b44dbeb60033d757971cfb2d3a4e1af287d38cc
maateh <39100142+maateh@users.noreply.github.com>
2024-10-06 18:05:56 +0200

    docs: write documentation for useTimer

--- src/hooks/use-timer.ts
+++ src/hooks/use-timer.ts
@@ -15,6 +15,21 @@ type UseTimerReturn = {
   timerInMs: number
 }
 
+/**
+ * A custom hook for managing a timer with configurable behavior.
+ * 
+ * - The timer can either increase or decrease over time.
+ * - Fires an `onUpdate` callback whenever the timer changes.
+ * 
+ * @param {Object} options - Configuration options for the timer.
+ * @param {TimerType} [options.type="INCREASE"] - Determines if the timer counts up or down.
+ * @param {number} [options.initialInMs=0] - Initial value of the timer in milliseconds.
+ * @param {number} [options.thresholdInMs=1000] - Amount to increase or decrease the timer per interval, in milliseconds.
+ * @param {number} [options.intervalInMs=1000] - Interval at which the timer updates, in milliseconds.
+ * @param {function} [options.onUpdate] - Callback function that receives the updated timer values after each interval.
+ * 
+ * @returns {UseTimerReturn} - Contains the current timer value in both seconds and milliseconds.
+ */
 export const useTimer = ({
   type = "INCREASE",
   initialInMs = 0,
@@ -24,12 +39,23 @@ export const useTimer = ({
 }: UseTimerProps = {}): UseTimerReturn => {
   const [timerInMs, setTimerInMs] = useState<number>(initialInMs)
 
+  /**
+   * Initialize 'onUpdate' callback. It needs to be done this way
+   * to prevent unnecessary retriggers if 'onUpdate' would change.
+   */
   const updateRef = useRef(onUpdate)
 
   useEffect(() => {
     updateRef.current = onUpdate
   }, [onUpdate])
 
+  /**
+   * Sets up the timer interval to update the timer value
+   * based on the type (increase or decrease).
+   * 
+   * Updates the timer every `intervalInMs` and triggers
+   * the `onUpdate` callback with the latest value.
+   */
   useEffect(() => {
     const intervalId = setInterval(() => {
       const threshold = type === 'INCREASE' ? thresholdInMs : -thresholdInMs
