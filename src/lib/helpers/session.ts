1928468549b0a9c3487c81770636fadecb3b3ab5
maateh <39100142+maateh@users.noreply.github.com>
2024-10-07 08:08:08 +0200

    docs: writing documentation for session helpers

--- src/lib/helpers/session.ts
+++ src/lib/helpers/session.ts
@@ -64,10 +64,20 @@ export function validateCardMatches(cards: PrismaJson.MemoryCard[]): ValidatedMe
 }
 
 /**
- * TODO: Write doc.
+ * Updates the game session statistics based on the given action.
  * 
- * @param param0 
- * @returns 
+ * Actions:
+ * - Flip: the 'flip' statistic counter for the current player
+ *   is incremented when one pair of cards is flipped. Flips are
+ *   only counted after the second card (one pair) is flipped.
+ * 
+ * - Match: the 'matches' statistic counter for the current player
+ *   is incremented when the player finds a pair of cards (that match).
+ * 
+ * @param {Object} session - The current game session, containing the players, stats, and flipped cards information.
+ * @param {'flip' | 'match'} [action] - Specifies whether the action performed is a card flip or a match.
+ * 
+ * @returns {PrismaJson.SessionStats} - The updated session statistics.
  */
 export function updateSessionStats(
   session: Pick<ClientGameSession, 'players' | 'stats' | 'flippedCards'>,
@@ -93,10 +103,15 @@ export function updateSessionStats(
 }
 
 /**
- * TODO: write doc
+ * Calculates the number of free flips available in the game session based on the session's type, mode, and table size.
  * 
- * @param session 
- * @returns 
+ * - If the session is of type 'CASUAL', no free flips are given, so the function returns `null`.
+ * - In 'COMPETITIVE', for 'SINGLE' and 'COOP' modes, the number of free flips
+ *   is determined by multiplying the table size by a predefined multiplier.
+ * 
+ * @param {Object} session - The game session containing the type, mode, and table size.
+ * 
+ * @returns {number | null} - The calculated free flips based on the session's settings, or `null` if not applicable.
  */
 export function getFreeFlips(
   session: Pick<ClientGameSession, 'type' | 'mode' | 'tableSize'>
@@ -118,11 +133,20 @@ export function getFreeFlips(
 }
 
 /**
- * TODO: write doc
+ * Calculates the score for a specific player in a game session based on the session's type, mode, and stats.
  * 
- * @param session 
- * @param playerTag 
- * @returns 
+ * NOTE: These calculations are likely not final and might be changed in the future.
+ * 
+ * - If the session type is 'CASUAL', scoring is not applicable, so the function returns `null`.
+ * - In 'SINGLE' mode, the score is calculated by comparing the player's number of flips with the allowed free flips.
+ *   - If the player has fewer or equal flips than the free flips, the score is equal to the free flips.
+ *   - If the player exceeds the free flips, the score is reduced by the difference between double the free flips and the player's flips.
+ * - Modes like 'PVP' and 'COOP' are not yet implemented and return `null`.
+ * 
+ * @param {Object} session - The game session containing type, mode, table size, and stats.
+ * @param {string} playerTag - The tag of the player whose score is being calculated.
+ * 
+ * @returns {number | null} - The player's calculated score or `null` if scoring is not applicable.
  */
 export function calculateSessionScore(
   session: Pick<ClientGameSession, 'type' | 'mode' | 'tableSize' | 'stats'>,
