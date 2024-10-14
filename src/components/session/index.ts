4c79b9eb0d0a3e46c1d3adfbab677b449ace047a
maateh <39100142+maateh@users.noreply.github.com>
2024-10-14 09:30:03 +0200

    feat(ui): add SessionStatusBadge component

--- src/components/session/index.ts
+++ src/components/session/index.ts
@@ -1,3 +1,4 @@
+export { default as SessionStatusBadge, SessionStatusMarker } from "./session-status-badge"
 export { default as SessionInfoBadge } from "./session-info-badge"
 export { default as SessionBasics } from "./session-basics"
 export { default as SessionStats } from "./session-stats"
