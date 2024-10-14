b15d9db78767b9abedb20f31134490f9ac4fcb9a
maateh <39100142+maateh@users.noreply.github.com>
2024-10-14 09:33:41 +0200

    style: separator added to CardItem

--- src/components/shared/card-item.tsx
+++ src/components/shared/card-item.tsx
@@ -1,12 +1,19 @@
 // utils
 import { cn } from "@/lib/utils"
 
+// shadcn
+import { Separator } from "@/components/ui/separator"
+
 const CardItem = ({ className, ...props }: React.ComponentProps<"li">) => {
   return (
-    <li className={cn(
-      "group w-full py-2.5 px-3 flex justify-between items-center rounded-xl transition hover:bg-transparent/5 dark:hover:bg-transparent/20",
-      className
-    )} {...props} />
+    <>
+      <li className={cn(
+        "group w-full py-2.5 px-3 flex justify-between items-center rounded-xl transition hover:bg-transparent/5 dark:hover:bg-transparent/20",
+        className
+      )} {...props} />
+
+      <Separator className="last:hidden w-5/6 mx-auto bg-border/10" />
+    </>
   )
 }
 
