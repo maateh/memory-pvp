2f0f661ed26dcf3fbcf43b2ae6212bd88a2b04e1
maateh <39100142+maateh@users.noreply.github.com>
2024-10-14 18:07:36 +0200

    feat(ui): display player stats on delete

--- src/components/player/card/player-delete-warning.tsx
+++ src/components/player/card/player-delete-warning.tsx
@@ -1,11 +1,27 @@
 "use client"
 
+import { useMemo } from "react"
+
 // types
 import type { PlayerProfile } from "@prisma/client"
 import type { DialogProps } from "@radix-ui/react-dialog"
+import type { Statistic } from "@/components/shared/statistics"
+
+// icons
+import { Hash, Star, Timer } from "lucide-react"
+
+// shadcn
+import { Separator } from "@/components/ui/separator"
 
 // components
-import { WarningActionButton, WarningCancelButton, WarningModal, WarningModalFooter } from "@/components/shared"
+import {
+  StatisticItem,
+  StatisticList,
+  WarningActionButton,
+  WarningCancelButton,
+  WarningModal,
+  WarningModalFooter
+} from "@/components/shared"
 
 // hooks
 import { useDeletePlayerMutation } from "@/lib/react-query/mutations/player"
@@ -17,13 +33,42 @@ type PlayerDeleteWarningProps = {
 const PlayerDeleteWarning = ({ player, ...props }: PlayerDeleteWarningProps) => {
   const { deletePlayer, handleDeletePlayer } = useDeletePlayerMutation()
 
+  const stats = useMemo<Statistic[]>(() => ([
+    {
+      Icon: Hash,
+      label: "Player Tag",
+      data: player.tag
+    },
+    {
+      Icon: Star,
+      label: "Total Score",
+      data: "50 points" // TODO: GET -> Total score
+    },
+    {
+      Icon: Timer,
+      label: "Playtime",
+      data: "1h 30sec" // TODO: GET -> Total playtime
+    }
+  ]), [player])
+
   return (
     <WarningModal
       title="Delete player profile"
       description="Are you sure you want to delete this player profile?"
       {...props}
     >
-      {/* TODO: display player stats here */}
+      <StatisticList className="max-w-md mx-auto">
+        {stats.map((stat) => (
+          <StatisticItem className="min-w-32 max-w-40 px-3 py-1.5 text-sm sm:text-sm"
+            iconProps={{ className: "size-4 sm:size-5" }}
+            dataProps={{ className: "text-xs" }}
+            statistic={stat}
+            key={stat.label}
+          />
+        ))}
+      </StatisticList>
+
+      <Separator className="w-1/5 mx-auto bg-border/15" />
 
       <WarningModalFooter>
         <WarningCancelButton onClick={() => props.onOpenChange && props.onOpenChange(false)}>
