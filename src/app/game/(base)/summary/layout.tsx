693f2e3111bffba72135bf434d103fba34f1f6c1
maateh <39100142+maateh@users.noreply.github.com>
2024-10-17 18:11:44 +0200

    refactor: move description into the layout
    Session summary description has been moved under the `BaseGameSummaryLayout`.

--- src/app/game/(base)/summary/layout.tsx
+++ src/app/game/(base)/summary/layout.tsx
@@ -12,9 +12,15 @@ const BaseGameSummaryLayout = ({ children }: BaseGameSummaryLayoutProps) => {
         <h1 className="pt-1 w-fit mx-auto text-3xl font-heading font-bold small-caps heading-decorator sm:text-5xl">
           Session Results
         </h1>
-      </header> 
 
-      <Separator className="w-3/4 mx-auto mt-4 mb-6 bg-border/50" />
+        <Separator className="w-3/4 mx-auto mt-4 mb-6 bg-border/40" />
+
+        <p className="-mt-3 max-w-lg mx-auto text-center font-heading text-base sm:text-xl">
+          Your <span className="text-accent font-medium">game is over</span>, thanks for playing!
+        </p>
+      </header>
+
+      <Separator className="w-3/5 mx-auto mt-4 mb-6 bg-border/5" />
 
       <main className="flex-1 mx-auto px-2.5 sm:px-4">
         {children}
