732ae169eb2bb054f44ac805713d779bc90fbece
maateh <39100142+maateh@users.noreply.github.com>
2024-10-18 14:21:17 +0200

    feat(ui): add new game `Link` to `/summary/layout`

--- src/app/game/(base)/summary/layout.tsx
+++ src/app/game/(base)/summary/layout.tsx
@@ -1,4 +1,13 @@
+import Link from "next/link"
+
+// utils
+import { cn } from "@/lib/utils"
+
+// icons
+import { Cog } from "lucide-react"
+
 // shadcn
+import { buttonVariants } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 
 type BaseGameSummaryLayoutProps = {
@@ -18,6 +27,17 @@ const BaseGameSummaryLayout = ({ children }: BaseGameSummaryLayoutProps) => {
         <p className="-mt-3 max-w-lg mx-auto text-center font-heading text-base sm:text-xl">
           Your <span className="text-accent font-medium">game is over</span>, thanks for playing!
         </p>
+
+        <Link className={cn(buttonVariants({
+          className: "w-fit mt-2 mx-auto flex gap-x-2 border-border/40 hover font-heading tracking-wider hover:bg-accent/15 dark:hover:bg-accent/10 hover:text-foreground",
+          variant: "outline",
+          size: "sm"
+        }))}
+          href="/game/setup"
+        >
+          <Cog className="size-4 sm:size-5" strokeWidth={1.75} />
+          <span className="mt-1">New game</span>
+        </Link>
       </header>
 
       <Separator className="w-3/5 mx-auto mt-4 mb-6 bg-border/5" />
