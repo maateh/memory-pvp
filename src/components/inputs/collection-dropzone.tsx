15c2f2149f430e37db34ce9c7065bc5a815f02a0
maateh <39100142+maateh@users.noreply.github.com>
2024-10-25 14:24:54 +0200

    refactor: remove unnecessary stuff from `CollectionDropzone`
    - removed `Button` (from ui) and `useUploadThing` hook because uploading will be handled on the form
    - renamed `fileUrls` state to `previewUrls`
    - show `maxFileCount` for the user

--- src/components/inputs/collection-dropzone.tsx
+++ src/components/inputs/collection-dropzone.tsx
@@ -3,63 +3,42 @@
 import { useCallback, useState } from "react"
 
 // types
-import type { UploadRouter } from "@/app/api/uploadthing/core"
+import type { ExpandedRouteConfig } from "uploadthing/types"
 
 // uploadthing
 import { generateClientDropzoneAccept, generatePermittedFileTypes } from "uploadthing/client"
-import { useDropzone } from "@uploadthing/react"
 
 // utils
 import { cn, generateFileUrls } from "@/lib/utils"
 
 // icons
-import { ImagePlus, ImageUp } from "lucide-react"
-
-// shadcn
-import { Button } from "@/components/ui/button"
+import { ImageUp } from "lucide-react"
 
 // copmponents
 import { CollectionPreviewDenseItem, CollectionPreviewDenseList } from "@/components/collection"
 
 // hooks
-import { useUploadThing } from "@/hooks/use-upload-thing"
+import { useDropzone } from "@uploadthing/react"
 
 type CollectionDropzoneProps = {
   files: File[]
   setFiles: (files: File[]) => void
-  endpoint: keyof UploadRouter
+  routeConfig?: ExpandedRouteConfig
   hidePreview?: boolean
-  hideUploadButton?: boolean
   className?: string
 }
 
-const CollectionDropzone = ({
-  files, setFiles, endpoint,
-  hidePreview = false, hideUploadButton = false,
-  className
-}: CollectionDropzoneProps) => {
-  const [fileUrls, setFileUrls] = useState<string[]>([])
+const CollectionDropzone = ({ files, setFiles, routeConfig, hidePreview = false, className }: CollectionDropzoneProps) => {
+  const [previewUrls, setPreviewUrls] = useState<string[]>([])
 
   const onDrop = useCallback(async (acceptedFiles: File[]) => {
     if (!hidePreview) {
       const urls = await generateFileUrls(acceptedFiles)
-      setFileUrls(urls)
+      setPreviewUrls(urls)
     }
 
     setFiles(acceptedFiles)
   }, [setFiles, hidePreview])
-
-  const { startUpload, routeConfig, isUploading } = useUploadThing(endpoint, {
-    onClientUploadComplete: () => {
-      console.info("files uploaded successfully")
-    },
-    onUploadError: () => {
-      console.error("error occurred while uploading")
-    },
-    onUploadBegin: (fileName) => {
-      console.info("upload has begun for: ", fileName)
-    }
-  })
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
@@ -70,6 +49,7 @@ const CollectionDropzone = ({
 
   const maxFileSize = routeConfig?.image?.maxFileSize
   const minFileCount = routeConfig?.image?.minFileCount
+  const maxFileCount = routeConfig?.image?.maxFileCount
 
   return (
     <div className={cn("px-4 py-8 flex flex-col items-center justify-center text-center rounded-2xl border border-input/50 border-dashed cursor-pointer", className)}
@@ -91,29 +71,19 @@ const CollectionDropzone = ({
 
         {minFileCount && (
           <p>
-            Selected <span className="font-bold">{files.length}</span> out of <span className="font-bold">{minFileCount}</span> images
+            Selected <span className="font-bold">{files.length}</span> out of <span className="font-bold">{minFileCount}</span> images (Max. <span className="font-bold">{maxFileCount}</span> possible)
           </p>
         )}
       </div>
 
       <CollectionPreviewDenseList className={cn({ "mt-4": files.length > 0, "hidden": hidePreview })}>
-        {fileUrls.map((url) => (
+        {previewUrls.map((url) => (
           <CollectionPreviewDenseItem
             imageUrl={url}
             key={url}
           />
         ))}
       </CollectionPreviewDenseList>
-
-      <Button className={cn("mt-4 gap-x-2", { "hidden": hideUploadButton })}
-        variant="secondary"
-        size="sm"
-        onClick={() => startUpload(files)}
-        disabled={isUploading || files.length !== minFileCount}
-      >
-        <ImagePlus className="size-3.5 sm:size-4" />
-        <span>Upload collection</span>
-      </Button>
     </div>
   )
 }
