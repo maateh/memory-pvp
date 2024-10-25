7dd71ce1dcd28ce4f52c8a1418d016a6d78f1f3e
maateh <39100142+maateh@users.noreply.github.com>
2024-10-25 21:03:41 +0200

    fix(type): fix tailwind config type problems

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config = withUt({
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -81,27 +81,9 @@ const config = withUt({
   				to: {
   					height: '0'
   				}
-  			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
   			}
   		},
   		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
