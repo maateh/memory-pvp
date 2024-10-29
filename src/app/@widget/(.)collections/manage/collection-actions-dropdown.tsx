acafc6bf492d5a28acb824702813b5911f16a818
maateh <39100142+maateh@users.noreply.github.com>
2024-10-29 13:53:33 +0100

    feat(ui): implement `/collections/manage` page
    - designed manage collections widget dialog
    - added dropdown to delete collections

---
+++ src/app/@widget/(.)collections/manage/collection-actions-dropdown.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { useState } from "react"
+
+// icons
+import { CloudCog,  ImageMinus, Loader2 } from "lucide-react"
+
+// shadcn
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu"
+import { Separator } from "@/components/ui/separator"
+
+// components
+import CollectionDeleteWarning from "./collection-delete-warning"
+
+// hooks
+import { useDeleteCollectionMutation } from "@/lib/react-query/mutations/collection"
+
+type CollectionActionsDropdownProps = {
+  collection: ClientCardCollection
+} & React.ComponentProps<typeof DropdownMenuTrigger>
+
+const CollectionActionsDropdown = ({ collection, ...props }: CollectionActionsDropdownProps) => {
+  const [showDeleteWarning, setShowDeleteWarning] = useState<boolean>(false)
+
+  const { deleteCollection } = useDeleteCollectionMutation()
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger {...props} asChild />
+
+        <DropdownMenuContent>
+          <DropdownMenuLabel className="flex items-center justify-between gap-x-8">
+            <div className="flex items-center gap-x-2">
+              <Separator className="h-4 w-1 rounded-full bg-border/50"
+                orientation="vertical"
+              />
+
+              <p className="pt-0.5 text-base font-normal font-heading tracking-wider">
+                Manage collection
+              </p>
+            </div>
+
+            <CloudCog className="size-4" strokeWidth={1.5} />
+          </DropdownMenuLabel>
+
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem
+              variant="destructive"
+              onClick={() => setShowDeleteWarning(true)}
+              disabled={deleteCollection.isPending}
+            >
+              {deleteCollection.isPending ? (
+                <Loader2 className="size-4 animate-spin" />
+              ) : (
+                <ImageMinus className="size-4" />
+              )}
+              <span>Delete collection</span>
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <CollectionDeleteWarning
+        collection={collection}
+        open={showDeleteWarning}
+        onOpenChange={() => setShowDeleteWarning(false)}
+      />
+    </>
+  )
+}
+
+export default CollectionActionsDropdown
