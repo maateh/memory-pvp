a69e6ab116c316d6c21d9d46623b12f8377e7f98
maateh <39100142+maateh@users.noreply.github.com>
2024-11-13 13:16:22 +0100

    chore: add next `Image` to `UserAvatar`
    Nothing changed in terms of usage, just added the `imageSize` props as optional.

--- src/components/user/user-avatar.tsx
+++ src/components/user/user-avatar.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 // types
 import type { LucideProps } from "lucide-react"
 
@@ -11,19 +13,30 @@ import { LogIn, UserRound } from "lucide-react"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 type UserAvatarProps = {
-  user: ClientUser | null
+  user: Pick<ClientUser, 'imageUrl' | 'username'> | null
+  imageSize?: number
   imageProps?: React.ComponentProps<typeof AvatarImage>
   fallbackProps?: React.ComponentProps<typeof AvatarFallback>
   fallbackIconProps?: LucideProps
 } & React.ComponentProps<typeof Avatar>
 
-const UserAvatar = ({ user, className, imageProps, fallbackProps, fallbackIconProps, ...props }: UserAvatarProps) => {
+const UserAvatar = ({ user, className, imageSize = 32, imageProps, fallbackProps, fallbackIconProps, ...props }: UserAvatarProps) => {
   return (
-    <Avatar className={cn("size-6", className)} {...props}>
+    <Avatar className={cn("size-6 img-wrapper", className)} {...props}>
       <AvatarImage {...imageProps}
         className={cn("rounded-full", imageProps?.className)}
         src={user?.imageUrl || undefined}
-      />
+        asChild
+      >
+        {user && user.imageUrl && (
+          <Image
+            src={user.imageUrl}
+            alt={`${user.username}'s avatar`}
+            height={imageSize}
+            width={imageSize}
+          />
+        )}
+      </AvatarImage>
       
       <AvatarFallback {...fallbackProps}
         className={cn("my-auto p-1 bg-transparent", fallbackProps?.className)}
