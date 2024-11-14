8c6539d677a330e251e62891082f59d2e0cb96bc
maateh <39100142+maateh@users.noreply.github.com>
2024-11-14 22:32:35 +0100

    refactor: move `useIsMobile` into `/hooks` folder
    Note: hook will be reused later.

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -23,14 +23,15 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 
+// hooks
+import { useIsMobile } from "@/hooks/use-is-mobile"
+
 const SIDEBAR_COOKIE_NAME = "sidebar:state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
 const SIDEBAR_WIDTH = "18rem"
 const SIDEBAR_WIDTH_MOBILE = "18rem"
 const SIDEBAR_WIDTH_ICON = "3rem"
 const SIDEBAR_KEYBOARD_SHORTCUT = "b"
-
-const MOBILE_BREAKPOINT = 768
 
 type SidebarContext = {
   state: "expanded" | "collapsed"
@@ -51,22 +52,6 @@ function useSidebar() {
   }
 
   return context
-}
-
-function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
-
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
-
-  return !!isMobile
 }
 
 const SidebarProvider = React.forwardRef<
