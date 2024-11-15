a1f2a47492fd727ef67b0d4c0fc83f2e8ce908e4
maateh <39100142+maateh@users.noreply.github.com>
2024-11-15 08:27:44 +0100

    docs: write doc to `useIsMobile`` hook

--- src/hooks/use-is-mobile.ts
+++ src/hooks/use-is-mobile.ts
@@ -3,11 +3,15 @@ import { useEffect, useState } from "react"
 const MOBILE_BREAKPOINT = 768
 
 /**
- * TODO: write doc
+ * Determines if the current screen width is below the mobile breakpoint.
  * 
- * Note: This custom hook comes from shadcn/ui.
- * 
- * @returns {boolean}
+ * **Note**: This custom hook comes from shadcn/ui.
+ *
+ * - Uses a media query to check if the viewport width is less than the specified `MOBILE_BREAKPOINT`.
+ * - Adds an event listener to update the `isMobile` state when the viewport changes.
+ * - Returns a boolean indicating whether the viewport is considered mobile-sized.
+ *
+ * @returns {boolean} - `true` if the viewport width is below the mobile breakpoint, otherwise `false`.
  */
 export function useIsMobile(): boolean {
   const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined)
