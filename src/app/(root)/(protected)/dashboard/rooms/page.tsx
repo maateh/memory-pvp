0a87df83c5465c0e807b9f92381aac4793cca435
maateh <39100142+maateh@users.noreply.github.com>
2024-11-19 22:23:15 +0100

    chore: remove waiting rooms widget modal route

--- src/app/(root)/(protected)/dashboard/rooms/page.tsx
+++ src/app/(root)/(protected)/dashboard/rooms/page.tsx
@@ -1,7 +1,14 @@
+// components
+import { Warning } from "@/components/shared"
+
 const WaitingRoomsPage = () => {
   // TODO: design this page, remove modal
   return (
-    <div>WaitingRoomsPage</div>
+    <Warning className="mt-32 mx-auto text-lg lg:text-xl font-heading"
+      messageProps={{ className: "mt-1" }}
+      iconProps={{ className: "lg:size-6" }}
+      message="Waiting rooms have not been implemented yet."
+    />
   )
 }
 
