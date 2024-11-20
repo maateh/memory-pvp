1f51994392591fdc50ef47c1ec7b11d054bfe58d
maateh <39100142+maateh@users.noreply.github.com>
2024-11-20 18:34:56 +0100

    feat(ui): add `CustomDate` shared component

---
+++ src/components/shared/custom-date.tsx
@@ -0,0 +1,29 @@
+// types
+import type { LucideIcon, LucideProps } from "lucide-react"
+
+// utils
+import { formatDistance } from "date-fns"
+import { cn } from "@/lib/utils"
+
+type CustomDateProps = {
+  date: Date
+  Icon: LucideIcon
+  iconProps?: LucideProps
+} & React.ComponentProps<"div">
+
+const CustomDate = ({ date, Icon, iconProps, className, ...props }: CustomDateProps) => {
+  return (
+    <div className={cn("flex items-center gap-x-1.5 text-foreground/85 text-xs", className)} {...props}>
+      <Icon {...iconProps}
+        className={cn("size-3 sm:size-3.5 shrink-0", iconProps?.className)}
+        strokeWidth={iconProps?.strokeWidth || 2.25}
+      />
+
+      <span>
+        {formatDistance(date, Date.now(), { addSuffix: true })}
+      </span>
+    </div>
+  )
+}
+
+export default CustomDate
