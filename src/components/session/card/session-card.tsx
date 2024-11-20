6553367dc682fba4c65f5b97909f425148edece6
maateh <39100142+maateh@users.noreply.github.com>
2024-11-20 21:41:02 +0100

    style: redesign `SessionCard`

--- src/components/session/card/session-card.tsx
+++ src/components/session/card/session-card.tsx
@@ -1,88 +1,83 @@
-import { formatDistance } from "date-fns"
-
 import Link from "next/link"
 
 // constants
 import { gameModePlaceholders, gameTypePlaceholders, tableSizePlaceholders } from "@/constants/game"
 
-// utils
-import { cn } from "@/lib/utils"
-
 // icons
-import { CalendarCheck, CalendarClock, Dices, ExternalLink, Gamepad2, Hash } from "lucide-react"
+import { CalendarCheck, CalendarClock, Dices, ExternalLink, Gamepad2, StepForward } from "lucide-react"
 
 // shadcn
-import { buttonVariants } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
 import { Skeleton } from "@/components/ui/skeleton"
 
 // components
 import { PlayerBadge } from "@/components/player"
-import { SessionInfoBadge, SessionStatusBadge } from "@/components/session"
+import { SessionInfoBadge, SessionBadge } from "@/components/session"
+import { CustomDate } from "@/components/shared"
 
 type SessionCardProps = {
   session: ClientGameSession
 }
 
 const SessionCard = ({ session }: SessionCardProps) => {
-  const date = session.closedAt || session.startedAt
-  const DateIcon = session.closedAt ? CalendarCheck : CalendarClock
-
   return (
-    <div className="w-full flex justify-between items-center">
-      <div className="flex items-center gap-x-3">
-        <SessionStatusBadge status={session.status} />
+    <div className="w-full py-1.5 px-1 sm:px-2 flex flex-wrap justify-between gap-x-3 gap-y-2">
+      <div className="space-y-2">
+        <SessionBadge className="w-fit"
+          session={session}
+        />
 
-        <div className="flex flex-col gap-y-2">
-          <div className="flex items-center gap-x-2">
-            <div className="w-fit px-2 flex items-center gap-x-1.5 bg-muted/75 rounded-lg">
-              <Hash className="size-3" strokeWidth={4} />
+        <div className="flex flex-wrap items-center gap-x-2 gap-y-1">
+          <SessionInfoBadge className="max-lg:px-2"
+            Icon={Gamepad2}
+            label={gameTypePlaceholders[session.type].label}
+            subLabel={gameModePlaceholders[session.mode].label}
+          />
 
-              <p className="font-heading font-light">
-                {session.slug}
-              </p>
-            </div>
-
-            <PlayerBadge
-              size="sm"
-              player={session.players.current}
-            />
-          </div>
-
-          <div className="flex flex-wrap items-center gap-x-2 gap-y-1">
-            <SessionInfoBadge
-              Icon={Gamepad2}
-              label={gameTypePlaceholders[session.type].label}
-              subLabel={gameModePlaceholders[session.mode].label}
-            />
-
-            <SessionInfoBadge
-              Icon={Dices}
-              label={tableSizePlaceholders[session.tableSize].label}
-              subLabel={tableSizePlaceholders[session.tableSize].size}
-            />
-          </div>
+          <SessionInfoBadge className="max-lg:px-2"
+            Icon={Dices}
+            label={tableSizePlaceholders[session.tableSize].label}
+            subLabel={tableSizePlaceholders[session.tableSize].size}
+          />
         </div>
       </div>
 
-      <div className="flex items-center gap-x-3.5">
-        <div className="flex items-center gap-x-1.5 text-foreground/85">
-          <DateIcon className="size-3 sm:size-3.5 flex-none" />
+      <div className="ml-auto flex flex-col items-end justify-between">
+        <div className="mb-1.5 flex flex-wrap-reverse items-center gap-x-1.5 gap-y-1">
+          <PlayerBadge player={session.players.current} />
 
-          <span className="text-xs small-caps">
-            {formatDistance(date, Date.now(), { addSuffix: true })}
-          </span>
+          {session.status === 'RUNNING' ? (
+            // TODO: continue session
+            <Button className="ml-auto p-2.5 rounded-full"
+              tooltip="Continue session (WIP)"
+              variant="ghost"
+              size="icon"
+            >
+              <StepForward className="size-4 sm:size-5 shrink-0 text-accent"
+                strokeWidth={2.5}
+              />
+            </Button>
+          ) : (
+            <Button className="ml-auto p-2.5 rounded-full"
+              tooltip="Open summary"
+              variant="ghost"
+              size="icon"
+              asChild
+            >
+              <Link href={`/game/summary/${session.slug}`}>
+                <ExternalLink className="size-4 sm:size-5 shrink-0 text-muted-foreground transition group-hover:text-foreground/90" />
+              </Link>
+            </Button>
+          )}
         </div>
 
-        <Link className={cn(buttonVariants({
-          className: "p-1.5 border border-border/10 rofull",
-          variant: "ghost",
-          size: "icon"
-        }))}
-          href={`/game/summary/${session.slug}`}
-          target="_blank"
-        >
-          <ExternalLink className="size-4 sm:size-5 text-muted-foreground transition group-hover:text-foreground/90" />
-        </Link>
+        <Separator className="w-11/12 ml-auto mt-auto mb-1.5 bg-border/65" />
+
+        <CustomDate className="text-end"
+          date={session.closedAt || session.startedAt}
+          Icon={session.closedAt ? CalendarCheck : CalendarClock}
+        />
       </div>
     </div>
   )
