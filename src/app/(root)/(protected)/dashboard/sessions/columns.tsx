d0c3f46723bd22cb287334da1733641661b1eb7a
maateh <39100142+maateh@users.noreply.github.com>
2024-11-20 21:41:55 +0100

    refactor: merge `slug` & `status` session table columns

--- src/app/(root)/(protected)/dashboard/sessions/columns.tsx
+++ src/app/(root)/(protected)/dashboard/sessions/columns.tsx
@@ -7,65 +7,37 @@ import type { ColumnDef } from "@tanstack/react-table"
 import { gameModePlaceholders, gameTypePlaceholders, tableSizePlaceholders } from "@/constants/game"
 
 // icons
-import { CalendarCheck, CalendarClock, Dices, ExternalLink, Gamepad2, Hash, Minus, StepForward } from "lucide-react"
+import { CalendarCheck, CalendarClock, Dices, ExternalLink, Gamepad2, Minus, StepForward } from "lucide-react"
 
 // shadcn
-import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { DataTableColumnSortingHeader, DataTableColumnToggle } from "@/components/ui/data-table"
-import { Separator } from "@/components/ui/separator"
 
 // components
 import { PlayerBadge } from "@/components/player"
-import { SessionInfoBadge, SessionStatusBadge } from "@/components/session"
+import { SessionInfoBadge, SessionBadge } from "@/components/session"
 import { CustomDate } from "@/components/shared"
 
 export const columns: ColumnDef<ClientGameSession>[] = [
   {
-    id: "Status",
+    id: "Session",
     accessorKey: "status",
     enableHiding: false,
     header() {
       return (
         <DataTableColumnSortingHeader
-          header="Status"
+          header="Session"
           sortValueKey="status"
         />
       )
     },
-    cell({ getValue }) {
-      const status = getValue<ClientGameSession['status']>()
+    cell({ row }) {
+      const session = row.original
 
-      // TODO: rename to `SessionStatusIndicator`
-      // TODO: add label
-      return <SessionStatusBadge status={status} />
-    },
-  },
-  {
-    id: "Slug",
-    accessorKey: "slug",
-    enableHiding: false,
-    header() {
       return (
-        <DataTableColumnSortingHeader
-          header="Slug"
-          sortValueKey="slug"
+        <SessionBadge className="2xl:text-base"
+          session={session}
         />
-      )
-    },
-    cell({ getValue }) {
-      const slug = getValue<ClientGameSession['slug']>()
-
-      return (
-        <Badge className="gap-x-2 text-sm 2xl:text-base text-foreground/80" variant="muted">
-          <Hash className="size-3.5" strokeWidth={4} />
-
-          <Separator className="w-1 h-4 bg-border/80 rounded-full" />
-
-          <p className="mt-0.5 font-heading font-normal">
-            {slug}
-          </p>
-        </Badge>
       )
     },
   },
