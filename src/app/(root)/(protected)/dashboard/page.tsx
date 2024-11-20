7e0c1f0f99de089ad8968793cc9e11e3c87179b0
maateh <39100142+maateh@users.noreply.github.com>
2024-11-20 13:30:37 +0100

    chore: remove `PlayerStatisticsWidgetCard`
    The functionality that would have been implemented here was merged into the `PlayerProfilesWidgetCard`.

--- src/app/(root)/(protected)/dashboard/page.tsx
+++ src/app/(root)/(protected)/dashboard/page.tsx
@@ -1,12 +1,11 @@
 // components
-import { PlayerStatisticsWidgetCard, PlayersWidgetCard } from "@/components/player/widget"
+import { PlayerProfilesWidgetCard } from "@/components/player/widget"
 import { SessionsWidgetCard, WaitingRoomsWidgetCard } from "@/components/session/widget"
 
 const DashboardPage = () => {
   return (
     <div className="page-wrapper widget-container">
-      <PlayerStatisticsWidgetCard />
-      <PlayersWidgetCard />
+      <PlayerProfilesWidgetCard />
       <WaitingRoomsWidgetCard />
       <SessionsWidgetCard />
     </div>
--- src/components/player/widget/index.ts
+++ src/components/player/widget/index.ts
@@ -1,3 +1,2 @@
 export { default as PlayerCreateWidgetCard } from "./player-create-widget-card"
 export { default as PlayerProfilesWidgetCard } from "./player-profiles-widget-card"
-export { default as PlayerStatisticsWidgetCard } from "./player-statistics-widget-card"
