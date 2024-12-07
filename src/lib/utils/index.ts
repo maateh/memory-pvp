f929ad45a68710894e79e84ff6227b305ddc4ea6
maateh <39100142+maateh@users.noreply.github.com>
2024-12-07 10:45:34 +0100

    chore: remove unused imports

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,14 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-import { DEFAULT_SERVER_ERROR_MESSAGE } from "next-safe-action"
-import { toast } from "sonner"
-
-// types
-import type { UploadThingError } from "uploadthing/server"
-import type { ActionError } from "@/server/actions/_error"
-import type { TRPCApiError } from "@/trpc/error"
-
 /** Tailwind class builder helper function (@shadcn/ui) */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
