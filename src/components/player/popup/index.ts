1ab770fa022027a2f9b1a8ab3fe525f82bf50ff7
maateh <39100142+maateh@users.noreply.github.com>
2024-12-14 11:21:42 +0100

    feat(ui): add player edit popup

--- src/components/player/popup/index.ts
+++ src/components/player/popup/index.ts
@@ -1,2 +1,3 @@
 export { default as PlayerSelectPopup } from "./player-select-popup"
+export { default as PlayerEditPopup } from "./player-edit-popup"
 export { default as PlayerDeletePopup } from "./player-delete-popup"
