42b7201df19d1ed0dd4f61f7376228b603112b55
maateh <39100142+maateh@users.noreply.github.com>
2025-01-06 11:34:39 +0100

    fix(db): declare prisma json types manually

--- packages/database/src/types.d.ts
+++ packages/database/src/types.d.ts
@@ -1,14 +1,29 @@
-import type { z } from "zod"
-import type { sessionCardMetadataSchema, sessionCardSchema, sessionStatsSchema } from "@repo/shared/schema/session"
-import type { playerStatsSchema } from "@repo/shared/schema/player"
-
-// FIXME: fix schema imports
-
 declare global {
   namespace PrismaJson {
-    type SessionCardMetadata = z.infer<typeof sessionCardMetadataSchema>
-    type SessionCard = z.infer<typeof sessionCardSchema>
-    type SessionStats = z.infer<typeof sessionStatsSchema>
-    type PlayerStats = z.infer<typeof playerStatsSchema>
+    type PlayerStats = {
+      score: number
+      timer: number
+      flips: number
+      matches: number
+      sessions: number
+    }
+
+    type SessionCardMetadata = {
+      id: string
+      key: number
+    }
+
+    type SessionCard = {
+      id: string
+      key: number
+      flippedBy: string | null
+      matchedBy: string | null
+    }
+
+    type SessionStats = {
+      timer: number
+      flips: Record<string, number>
+      matches: Record<string, number>
+    }
   }
 }
