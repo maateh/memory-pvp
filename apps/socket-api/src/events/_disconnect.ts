8ab63dd72a509dd3d1f64a1d45c125d08b63c664
maateh <39100142+maateh@users.noreply.github.com>
2025-01-29 14:11:58 +0100

    feat(api): handle client disconnection
    Note: has been implemented only in case of `"waiting"` and `"joined"` room statuses.

--- apps/socket-api/src/events/_disconnect.ts
+++ apps/socket-api/src/events/_disconnect.ts
@@ -1,21 +1,80 @@
+// types
+import type { JoinedRoom, WaitingRoom, SessionRoom } from "@repo/schema/session-room"
+
 // socket
 import { io } from "@/server"
 
 // redis
-import { redis } from "@repo/redis"
+import { redis } from "@/redis"
+import { getPlayerConnection } from "@/redis/connection-commands"
+import { getSessionRoom } from "@/redis/room-commands"
+
+// config
+import { connectionKey, roomKey, waitingRoomKey, waitingRoomsKey } from "@repo/config/redis-keys"
 
 export const disconnect: SocketEventHandler = (socket) => async () => {
   console.info("DEBUG - disconnect -> ", socket.id)
 
   try {
-    const sessionKey = await redis.hget(`memory:connections:${socket.id}`, "roomSlug")
-    await redis.del(`memory:connections:${socket.id}`)
-    await redis.del(`memory:session_rooms:${sessionKey}`)
-  
-    // TODO: close game session properly
-    io.to(socket.id).emit("room:left", {
-      message: "USER has disconnected."
-    }) 
+    const { roomSlug, playerId } = await getPlayerConnection(socket.id)
+    const room = await getSessionRoom<WaitingRoom | JoinedRoom | SessionRoom>(roomSlug, true)
+
+    if (room.status === "waiting") {
+      await Promise.all([
+        redis.del(connectionKey(socket.id)),
+        redis.del(waitingRoomKey(roomSlug)),
+        redis.lrem(waitingRoomsKey, 1, roomSlug)
+      ])
+    }
+
+    if (room.status === "joined") {
+      const ownerDisconnected = room.owner.id === playerId
+      const { guest: _, ...joinedRoom } = room as JoinedRoom
+      const waitingRoom: WaitingRoom = { ...joinedRoom, status: "waiting" }
+
+      if (ownerDisconnected) {
+        await Promise.all([
+          redis.del(connectionKey(room.owner.socketId)),
+          redis.del(connectionKey(room.guest.socketId)),
+          redis.del(roomKey(roomSlug))
+        ])
+      } else {
+        await Promise.all([
+          redis.del(connectionKey(socket.id)),
+          redis.del(roomKey(roomSlug)),
+          redis.hset(waitingRoomKey(roomSlug), waitingRoom),
+          redis.lpush(waitingRoomsKey, roomSlug)
+        ])
+      }
+
+      socket.broadcast.to(roomSlug).emit(`room:${ownerDisconnected ? "closed" : "left"}`, {
+        success: true,
+        message: `${playerId} has disconnected.`, // TODO: update `SocketPlayerConnection`
+        data: ownerDisconnected ? null : waitingRoom
+      } satisfies SocketResponse<WaitingRoom | null>)
+    }
+
+    if (room.status === "ready" || room.status === "starting") {
+      // TODO:
+      // - remove both joined players connection
+      // - remove session room
+      // - need to handle deleting the session from the database that might have already been created
+      // - Note: from this point deducting points is reasonable (if the game session was COMPETITIVE)
+      // - Note: (first create and then) emit `session:cancelled` event
+    }
+
+    if (room.status === "running") {
+      // TODO:
+      // - remove both joined players connection
+      // - remove session room
+      // - need to handle deleting the session from the database
+      // - deduct points from the player who has left (if the game session was COMPETITIVE)
+      // - Note: (first create and then) emit `session:cancelled` event
+    }
+
+    if (room.status === "finished") {
+      // Note: i guess nothing should be done here, so it's just a reminder to think about it later
+    }
   } catch (err) {
     console.error(err)
   }
