776c32126be4c37409f85a13f6c1e499d06a22e6
maateh <39100142+maateh@users.noreply.github.com>
2025-02-20 14:50:18 +0100

    chore(render): replace `session-not-found` with `redirect-fallback`

--- apps/next-app/src/app/game/(base)/summary/[slug]/page.tsx
+++ apps/next-app/src/app/game/(base)/summary/[slug]/page.tsx
@@ -1,6 +1,6 @@
-import dynamic from "next/dynamic"
+import { Suspense } from "react"
 
-// server
+// db
 import { getClientSession } from "@/server/db/query/session-query"
 
 // utils
@@ -11,13 +11,9 @@ import { cn } from "@/lib/util"
 import { Separator } from "@/components/ui/separator"
 
 // components
+import { Await, RedirectFallback } from "@/components/shared"
 import { SessionStatistics, SessionStatisticsSkeleton } from "@/components/session/summary"
 import SessionPlayerStats from "./session-player-stats"
-
-const SessionNotFound = dynamic(() => import('./session-not-found'), {
-  ssr: false,
-  loading: SessionStatisticsSkeleton
-})
 
 type SessionSummaryPageProps = {
   params: {
@@ -26,34 +22,41 @@ type SessionSummaryPageProps = {
 }
 
 const SessionSummaryPage = async ({ params }: SessionSummaryPageProps) => {
-  const clientSession = await getClientSession({ slug: params.slug })
-
-  if (!clientSession) {
-    return <SessionNotFound />
-  }
-
   return (
-    <>
-      <SessionStatistics stats={getRendererSessionStats(clientSession)} />
+    <Suspense fallback={<SessionStatisticsSkeleton />}>
+      <Await promise={getClientSession({ slug: params.slug })}>
+        {(session) => session ? (
+          <>
+            <SessionStatistics stats={getRendererSessionStats(session)} />
 
-      <Separator className="w-2/5 mx-auto mt-8 mb-12 bg-border/10" />
+            <Separator className="w-2/5 mx-auto mt-8 mb-12 bg-border/10" />
 
-      <div className={cn("mx-auto grid gap-x-16 gap-y-12", {
-        "lg:grid-cols-2": clientSession.mode !== "SINGLE"
-      })}>
-        <SessionPlayerStats
-          player={clientSession.owner}
-          session={clientSession}
-        />
+            <div className={cn("mx-auto grid gap-x-16 gap-y-12", {
+              "lg:grid-cols-2": session.mode !== "SINGLE"
+            })}>
+              <SessionPlayerStats
+                player={session.owner}
+                session={session}
+              />
 
-        {clientSession.mode !== "SINGLE" && (
-          <SessionPlayerStats
-            player={clientSession.guest}
-            session={clientSession}
+              {session.mode !== "SINGLE" && (
+                <SessionPlayerStats
+                  player={session.guest}
+                  session={session}
+                />
+              )}
+            </div>
+          </>
+        ) : (
+          <RedirectFallback
+            redirect="/game/setup"
+            type="replace"
+            message="Session not found."
+            description="Sorry, but the session cannot be found or you don't have access to it."
           />
         )}
-      </div>
-    </>
+      </Await>
+    </Suspense>
   )
 }
 
