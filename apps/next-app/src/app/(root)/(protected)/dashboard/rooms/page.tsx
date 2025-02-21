64df92cd655de2cd2b8a896104b0546fcd21da82
maateh <39100142+maateh@users.noreply.github.com>
2025-02-21 20:28:53 +0100

    chore(ui): display active session on the room listing page

--- apps/next-app/src/app/(root)/(protected)/dashboard/rooms/page.tsx
+++ apps/next-app/src/app/(root)/(protected)/dashboard/rooms/page.tsx
@@ -4,43 +4,53 @@ import { Suspense } from "react"
 import { getPlayer } from "@/server/db/query/player-query"
 
 // redis
-import { getWaitingRooms } from "@repo/server/redis-commands"
+import { getActiveRoom, getWaitingRooms } from "@repo/server/redis-commands"
+
+// shadcn
+import { Separator } from "@/components/ui/separator"
 
 // components
 import { Await, RedirectFallback } from "@/components/shared"
+import { ActiveRoom } from "@/components/room"
 import { WaitingRoomListing, WaitingRoomListingSkeleton } from "@/components/room/listing"
 
 const WaitingRoomsPage = async () => {
-  const promises = Promise.all([
-    getPlayer({
-      filter: { isActive: true },
-      withAvatar: true
-    }),
-    // TODO: add filter/sort/pagination options
-    getWaitingRooms()
-  ])
-
   return (
     <Suspense fallback={<WaitingRoomListingSkeleton />}>
-      <Await promise={promises}>
-        {([player, rooms]) => {
-          if (!player) {
-            return (
-              <RedirectFallback
-                type="replace"
-                message="Active player profile not found."
-                description="Please create a player profile first."
-              />
-            )
-          }
+      <Await promise={getPlayer({
+        filter: { isActive: true },
+        withAvatar: true
+      })}>
+        {(player) => player ? (
+          <Await promise={Promise.all([
+            getActiveRoom(player.id),
+            // TODO: add filter/sort/pagination options
+            getWaitingRooms()
+          ])}>
+            {([activeRoom, rooms]) => (
+              <>
+                {activeRoom && (
+                  <>
+                    <ActiveRoom room={activeRoom} />
 
-          return (
-            <WaitingRoomListing
-              guestPlayer={player}
-              rooms={rooms}
-            />
-          )
-        }}
+                    <Separator className="my-6 bg-border/40" />
+                  </>
+                )}
+
+                <WaitingRoomListing
+                  guestPlayer={player}
+                  rooms={rooms}
+                />
+              </>
+            )}
+          </Await>
+        ) : (
+          <RedirectFallback
+            type="replace"
+            message="Active player profile not found."
+            description="Please create a player profile first."
+          />
+        )}
       </Await>
     </Suspense>
   )
