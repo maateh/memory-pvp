4bac630927766824ecf4b9e0075ef17c3aea878b
maateh <39100142+maateh@users.noreply.github.com>
2025-02-24 18:15:55 +0100

    fix: remove `signedIn` from `/game/(base)/layout`

--- apps/next-app/src/app/game/(base)/layout.tsx
+++ apps/next-app/src/app/game/(base)/layout.tsx
@@ -1,11 +1,8 @@
 import Link from "next/link"
 import dynamic from "next/dynamic"
 
-// server
-import { signedIn } from "@/server/action/user-action"
-
 // icons
-import { Home, LayoutDashboard } from "lucide-react"
+import { LayoutDashboard } from "lucide-react"
 
 // shadcn
 import { Button } from "@/components/ui/button"
@@ -22,11 +19,7 @@ type BaseGameLayoutProps = {
   children: React.ReactNode
 }
 
-const BaseGameLayout = async ({ children }: BaseGameLayoutProps) => {
-  const user = await signedIn()
-
-  const NavigationIcon = user ? LayoutDashboard : Home
-
+const BaseGameLayout = ({ children }: BaseGameLayoutProps) => {
   return (
     <div className="relative flex-1 pt-16 pb-8 px-4 flex flex-col">
       <Button className="bg-accent/30 hover:bg-accent/40 font-normal dark:font-light tracking-wide expandable absolute top-3 left-3 sm:top-4 sm:left-4"
@@ -35,9 +28,9 @@ const BaseGameLayout = async ({ children }: BaseGameLayoutProps) => {
         asChild
       >
         <Link href="/">
-          <NavigationIcon className="size-4 sm:size-5 shrink-0" />
+          <LayoutDashboard className="size-4 sm:size-5 shrink-0" />
           <div className="ml-1 sm:ml-1.5">
-            {user ? 'Dashboard' : 'Homepage'}
+            Dashboard
           </div>
         </Link>
       </Button>
