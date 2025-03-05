7c272126c0faca49ae8df91b0f680b03c3333f19
maateh <39100142+maateh@users.noreply.github.com>
2025-03-05 15:17:55 +0100

    fix(type): fix `ClerkUser` type
    It's a mystery how this went wrong.

--- apps/next-app/src/lib/types/clerk.ts
+++ apps/next-app/src/lib/types/clerk.ts
@@ -1,4 +1,4 @@
 import type { UserResource } from "@clerk/types"
 
 /* User types */
-export type ClerkUser = Omit<UserResource, 'username'> & { username: string }
+export type ClerkUser = Pick<UserResource, "username" | "imageUrl"> & { username: string }
