a8939d168bf6250582a9d7a00e51ddd01e6c7434
maateh <39100142+maateh@users.noreply.github.com>
2025-03-11 09:32:00 +0100

    chore(server): make `ServerError` (data field) generic

--- packages/server/src/error/index.ts
+++ packages/server/src/error/index.ts
@@ -2,16 +2,16 @@
 import type { CauseKey, ThrownBy, ServerErrorOpts } from "@/error/types"
 import type { ExtendedSocketError } from "@/types/socket"
 
-export class ServerError {
+export class ServerError<T = unknown> {
   public readonly thrownBy: ThrownBy
   public readonly key: CauseKey
   public readonly message: string
   public readonly description?: string | undefined
-  public readonly data?: unknown
+  public readonly data?: T
 
   public readonly name: "ServerError"
 
-  constructor(opts: ServerErrorOpts) {
+  constructor(opts: ServerErrorOpts<T>) {
     this.thrownBy = opts.thrownBy || "UNKNOWN"
     this.key = opts.key || "UNKNOWN"
     this.data = opts.data
@@ -21,33 +21,33 @@ export class ServerError {
     this.name = "ServerError"
   }
 
-  static throw(error: ServerErrorOpts): never {
-    throw new ServerError(error)
+  static throw<T = unknown>(error: ServerErrorOpts<T>): never {
+    throw new ServerError<T>(error)
   }
 
-  static throwInAction(error: Omit<ServerErrorOpts, "thrownBy">, message?: string): never {
+  static throwInAction<T = unknown>(error: Omit<ServerErrorOpts<T>, "thrownBy">, message?: string): never {
     throw new Error(message, {
-      cause: new ServerError({ ...error, thrownBy: "ACTION" })
+      cause: new ServerError<T>({ ...error, thrownBy: "ACTION" })
     })
   }
 
-  static parser(error: ServerError | unknown): ServerError {
+  static parser<T = unknown>(error: ServerError<T> | unknown): ServerError<T> {
     if (error instanceof ServerError) {
       return error
     }
 
-    if (error && (error as ServerError).name === "ServerError") {
-      return error as ServerError
+    if (error && (error as ServerError<T>).name === "ServerError") {
+      return error as ServerError<T>
     }
 
-    return new ServerError({
+    return new ServerError<T>({
       message: "Something went wrong.",
       description: "Something unexpected happened during this request. Please try again later."
     })
   }
 
-  static asSocketError(error: ServerError | unknown): ExtendedSocketError<ServerError> {
-    const serverError = ServerError.parser(error)
+  static asSocketError<T = unknown>(error: ServerError<T> | unknown): ExtendedSocketError<ServerError<T>> {
+    const serverError = ServerError.parser<T>(error)
 
     const socketError: ExtendedSocketError = new Error(serverError.message)
     socketError.data = serverError
