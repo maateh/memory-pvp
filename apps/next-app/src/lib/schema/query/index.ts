c1bdb3bf92e67aed30fbd8dc94ce78ec3c1c6d87
maateh <39100142+maateh@users.noreply.github.com>
2025-03-14 21:08:11 +0100

    chore(client): remove unused schemas and types

--- apps/next-app/src/lib/schema/query/index.ts
+++
@@ -1,3 +0,0 @@
-import { z } from "zod"
-
-export const sortKeys = z.enum(['asc', 'desc']).optional()
--- apps/next-app/src/lib/types/query.ts
+++
@@ -1,6 +0,0 @@
-/* Filter types */
-export type Filter<T> = Partial<{ [key in keyof T]: T[key] }>
-
-/* Sort types */
-export type SortKey = "asc" | "desc"
-export type Sort<T> = Partial<{ [key in keyof T]: SortKey }>
