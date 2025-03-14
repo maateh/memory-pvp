5382e03c88674d9ab459d8178eb218bf1584c958
maateh <39100142+maateh@users.noreply.github.com>
2025-03-14 21:22:43 +0100

    feat(parser): validate game setup search params

--- apps/next-app/src/app/game/(base)/setup/@popup/(.)warning/page.tsx
+++ apps/next-app/src/app/game/(base)/setup/@popup/(.)warning/page.tsx
@@ -3,7 +3,11 @@ import dynamic from "next/dynamic"
 // types
 import type { MatchFormat } from "@repo/db"
 
+// schemas
+import { MatchFormatSchema } from "@repo/db/zod"
+
 // components
+import { RedirectFallback } from "@/components/shared"
 import { PopupLoader } from "@/components/popup"
 import { SessionRunningPopup } from "@/components/session/popup"
 
@@ -19,6 +23,21 @@ type SessionRunningPopupPageProps = {
 }
 
 const SessionRunningPopupPage = ({ searchParams }: SessionRunningPopupPageProps) => {
+  const {
+    success,
+    data: validatedParams
+  } = MatchFormatSchema.safeParse(searchParams.format)
+
+  if (!success || !validatedParams) {
+    return (
+      <RedirectFallback
+        type="back"
+        message="Failed to validate search parameters."
+        description="Please do not modify the search parameters manually."
+      />
+    )
+  }
+
   if (searchParams.format === "OFFLINE") {
     return <OfflineSessionWarningPopup />
   }
