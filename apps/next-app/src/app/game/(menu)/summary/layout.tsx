a1cfc16d5a62fdb34c16462c8735f63697ad797d
maateh <39100142+maateh@users.noreply.github.com>
2025-03-28 11:31:26 +0100

    style: redesign game summary layout

--- apps/next-app/src/app/game/(menu)/summary/layout.tsx
+++ apps/next-app/src/app/game/(menu)/summary/layout.tsx
@@ -1,47 +1,52 @@
 import Link from "next/link"
 
 // icons
-import { Cog } from "lucide-react"
+import { ArrowBigLeftDash } from "lucide-react"
 
 // shadcn
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 
+// components
+import { GlowingOverlay } from "@/components/shared"
+
 const GameSummaryLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <header className="mt-4">
-        <h1 className="pt-1 w-fit mx-auto text-3xl font-heading font-bold small-caps heading-decorator sm:text-5xl">
+      <header className="mt-4 mb-6">
+        <h1 className="w-fit mx-auto text-3xl sm:text-4xl font-heading font-bold small-caps heading-decorator">
           Session Results
         </h1>
 
-        <Separator className="w-3/4 mx-auto mt-4 mb-6 bg-border/40" />
+        <Separator className="w-1/5 mx-auto mt-1 mb-4 bg-border/10" />
 
-        <p className="-mt-3 max-w-lg mx-auto text-center font-heading text-base sm:text-xl">
-          Your <span className="text-accent font-medium">game is over</span>, thanks for playing!
+        <p className="-mt-3 max-w-lg mx-auto font-heading text-muted-foreground text-center text-base sm:text-xl">
+          The <span className="text-accent font-medium">game is over</span>, thanks for playing!
         </p>
-
-        <Button className="w-fit mt-2 mx-auto flex gap-x-2 border-border/40 hover font-heading tracking-wider hover:bg-accent/15 dark:hover:bg-accent/10 hover:text-foreground"
-          variant="outline"
-          size="sm"
-          asChild
-        >
-          <Link href="/game/setup">
-            <Cog className="size-4 sm:size-5 shrink-0"
-              strokeWidth={1.75}
-            />
-
-            <span className="mt-1">
-              New game
-            </span>
-          </Link>
-        </Button>
       </header>
 
-      <Separator className="w-3/5 mx-auto mt-4 mb-6 bg-border/5" />
-
-      <main className="flex-1 mx-auto px-2.5 sm:px-4">
+      <main className="flex-1 flex flex-col mx-auto px-2.5 sm:px-4">
         {children}
+
+        <GlowingOverlay className="w-32 sm:w-44 mt-auto mx-auto py-2.5"
+          overlayProps={{ className: "bg-accent opacity-90 dark:opacity-50" }}
+        >
+          <Button className="z-10 relative mx-auto flex gap-x-2 rounded-full text-foreground/90 font-heading tracking-wider sm:text-base"
+            variant="ghost"
+            size="icon"
+            asChild
+          >
+            <Link href="/game/setup">
+              <ArrowBigLeftDash className="size-5 sm:size-6 shrink-0"
+                strokeWidth={1.75}
+              />
+
+              <span className="mt-1">
+                New game
+              </span>
+            </Link>
+          </Button>
+        </GlowingOverlay>
       </main>
     </>
   )
