15c761c340c63ef0f9d5fc44da2fe27720ba610b
maateh <39100142+maateh@users.noreply.github.com>
2025-03-28 09:12:44 +0100

    chore(ui): remove redundant input components

--- apps/next-app/src/components/input/index.ts
+++ apps/next-app/src/components/input/index.ts
@@ -1,6 +1,5 @@
 export { default as ColorPicker } from "./color-picker"
 export { UploadButton, UploadDropzone } from "./_uploadthing"
 export { default as CollectionDropzone } from "./collection-dropzone"
-export { default as DropdownRadioSelect } from "./dropdown-radio-select"
 export { default as CommandSelect } from "./command-select"
 export { default as PopoverSelect } from "./popover-select"
